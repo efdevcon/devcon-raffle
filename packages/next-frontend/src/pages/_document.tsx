5dcd81f0518c61ca1a6561151503fece5ac8c055
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-05 10:15:24 +0200

    Allow styled-components

--- packages/next-frontend/src/pages/_document.tsx
+++ packages/next-frontend/src/pages/_document.tsx
@@ -1,6 +1,7 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps} from "next/document";
+import {ServerStyleSheet} from "styled-components";
 
-export default function Document() {
+export default function AppDocument() {
   return (
     <Html lang="en">
       <Head />
@@ -10,4 +11,24 @@ export default function Document() {
       </body>
     </Html>
   );
+}
+
+AppDocument.getInitialProps = async (ctx: DocumentContext): Promise<DocumentInitialProps> => {
+  const sheet = new ServerStyleSheet()
+  const originalRenderPage = ctx.renderPage
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      })
+
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: [initialProps.styles, sheet.getStyleElement()],
+    }
+  } finally {
+    sheet.seal()
+  }
 }
