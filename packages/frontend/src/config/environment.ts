c3344cc48acaa96b8fa83dbbe83271d63da15d98
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-12 10:27:02 +0200

    Add env variables

---
+++ packages/frontend/src/config/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  voucherRedeemDeadline: getDateEnv(process.env.NEXT_VOUCHER_REDEEM_DEADLINE)
+}
+
+function getDateEnv(envValue: string | undefined): number | undefined {
+  return envValue ? new Date(envValue).getTime() / 1000 : undefined
+}
+
