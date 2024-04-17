bbe7a2f57fc6e6f27821e0f54bf84030777de34d
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-17 14:21:20 +0200

    Set correct hardhat address

--- packages/frontend/src/blockchain/auctionAddresses.ts
+++ packages/frontend/src/blockchain/auctionAddresses.ts
@@ -5,7 +5,7 @@ import { arbitrum, arbitrumSepolia, hardhat } from 'wagmi/chains'
 export const AUCTION_ADDRESSES: Record<SupportedChainId, Hex> = {
   [arbitrum.id]: '0xF53d383525117d1f51BF234966E39bD1508a5948',
   [arbitrumSepolia.id]: '0xF53d383525117d1f51BF234966E39bD1508a5948',
-  [hardhat.id]: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+  [hardhat.id]: '0xa513e6e4b8f2a923d98304ec87f64353c4d5c853',
 }
 
 export const DEPLOYMENT_BLOCK: Record<SupportedChainId, bigint> = {
