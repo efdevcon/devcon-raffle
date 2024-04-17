d2c191a547c0bb3f9d6da1fd2e30e992c6958665
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-17 08:37:40 +0200

    Add useUserBid hook

---
+++ packages/frontend/src/blockchain/hooks/useUserBid.ts
@@ -0,0 +1,21 @@
+import { useAccount } from "wagmi";
+import { useBids } from "@/providers/BidsProvider";
+import { useMemo } from "react";
+import { UserBid } from "@/types/bid";
+
+export const useUserBid = (): UserBid | undefined => {
+  const { address } = useAccount()
+  const { bidList } = useBids()
+
+  return useMemo(() => {
+    if (!address) {
+      return undefined
+    }
+
+    const userBidPlace = bidList.findIndex((bid) => bid.address === address)
+    return userBidPlace > 0 ? {
+      ...bidList[userBidPlace],
+      place: userBidPlace,
+    } : undefined
+  }, [address, bidList])
+}
