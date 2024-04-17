04c9d5fb33b6b40e04546b63cbdf54c59a9d364b
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-17 14:30:25 +0200

    Correctly handle user bid in useUserBid

--- packages/frontend/src/blockchain/hooks/useUserBid.ts
+++ packages/frontend/src/blockchain/hooks/useUserBid.ts
@@ -13,10 +13,10 @@ export const useUserBid = (): BidWithPlace | undefined => {
     }
 
     const userBidPlace = bidList.findIndex((bid) => bid.address === address)
-    return userBidPlace > 0
+    return userBidPlace >= 0
       ? {
           ...bidList[userBidPlace],
-          place: userBidPlace,
+          place: userBidPlace + 1,
         }
       : undefined
   }, [address, bidList])
