6ea254ebf30792506c70afa14b4573cbae877181
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2024-04-17 14:25:28 +0200

    🪄 Support Hardhat chain in frontend app (#16)

--- packages/frontend/src/config/wagmiConfig.ts
+++ packages/frontend/src/config/wagmiConfig.ts
@@ -7,7 +7,7 @@ export const wagmiConfig = createConfig({
   transports: {
     [arbitrum.id]: webSocket(`wss://arbitrum-mainnet.infura.io/ws/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`),
     [arbitrumSepolia.id]: webSocket(`wss://arbitrum-sepolia.infura.io/ws/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`),
-    [hardhat.id]: webSocket('http://127.0.0.1:8545'),
+    [hardhat.id]: webSocket('ws://127.0.0.1:8545'),
   },
 })
 
