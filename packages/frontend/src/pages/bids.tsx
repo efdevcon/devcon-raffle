7cde73db52067af360c1e46b4623a1a1c9a7357f
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-18 09:40:01 +0200

    Add bids page

---
+++ packages/frontend/src/pages/bids.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { Colors } from "@/styles/colors";
+import { Header } from "@/components/allBids/Header";
+
+export default function Bids() {
+  return (
+    <Body>
+      <Header />
+    </Body>
+  )
+}
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  width: 100%;
+  background: ${Colors.GreyLight};
+`
