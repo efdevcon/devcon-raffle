a0b2fc9cd9961da11f703e0c0f73fb77943e067f
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2024-04-19 12:45:53 +0200

    🪄 Utilize getBidsWithAddresses function (#19)

--- packages/frontend/src/providers/BidsProvider/useWatchEvents.ts
+++ packages/frontend/src/providers/BidsProvider/useWatchEvents.ts
@@ -1,46 +1,37 @@
-import { parseAbiItem } from 'viem'
-import { BidEventsState } from '@/providers/BidsProvider/reduceBids'
-import { useBlockNumber, useChainId, useConfig, useWatchContractEvent } from 'wagmi'
-import { useQuery } from '@tanstack/react-query'
-import { getLogs } from 'viem/actions'
-import { AUCTION_ADDRESSES, DEPLOYMENT_BLOCK } from '@/blockchain/auctionAddresses'
+import { useChainId, useReadContract, useWatchContractEvent } from 'wagmi'
+import { AUCTION_ADDRESSES } from '@/blockchain/auctionAddresses'
 import { AUCTION_ABI } from '@/blockchain/abi/auction'
+import { useEffect } from 'react'
+import { ReduceBidsAction } from '@/providers/BidsProvider/reduceBids'
 
-const newBidEvent = parseAbiItem('event NewBid(address bidder, uint256 bidderID, uint256 bidAmount)')
-
-export const useWatchEvents = (onEvents: (eventsState: BidEventsState) => void) => {
+export const useWatchEvents = (dispatch: (eventsState: ReduceBidsAction) => void) => {
   const chainId = useChainId()
-  const config = useConfig()
-  const client = config.getClient({ chainId })
 
-  const { data: blockNumber, isLoading: isBlockLoading } = useBlockNumber()
-
-  const { isLoading: areInitialBidsLoading } = useQuery({
-    queryKey: ['bids', chainId],
-    queryFn: async () => {
-      const logs = await getLogs(client, {
-        address: AUCTION_ADDRESSES[chainId],
-        event: newBidEvent,
-        fromBlock: DEPLOYMENT_BLOCK[chainId],
-        toBlock: blockNumber,
-      })
-      onEvents({ events: logs, chainId, startBlock: blockNumber })
-      return logs
-    },
-    enabled: !isBlockLoading,
-    staleTime: Infinity,
-    gcTime: Infinity,
+  const { data, isLoading: areInitialBidsLoading } = useReadContract({
+    chainId,
+    abi: AUCTION_ABI,
+    address: AUCTION_ADDRESSES[chainId],
+    functionName: 'getBidsWithAddresses',
   })
+
+  useEffect(() => {
+    if (!data) {
+      return
+    }
+    dispatch({
+      type: 'InitialBids',
+      bids: data,
+    })
+  }, [data, dispatch])
 
   useWatchContractEvent({
     chainId,
     abi: AUCTION_ABI,
     address: AUCTION_ADDRESSES[chainId],
-    fromBlock: blockNumber,
     eventName: 'NewBid',
-    onLogs: (logs) => onEvents({ events: logs, chainId, startBlock: blockNumber }),
-    enabled: !isBlockLoading && !areInitialBidsLoading,
+    onLogs: (logs) => dispatch({ type: 'NewBids', events: logs }),
+    enabled: !areInitialBidsLoading,
   })
 
-  return { isLoading: isBlockLoading || areInitialBidsLoading }
+  return { isLoading: areInitialBidsLoading }
 }
