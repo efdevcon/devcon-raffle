d04e4744af9f995f95f8d164738b33abe79f4fdf
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-22 16:32:47 +0200

    Add TopBar as layout

---
+++ packages/frontend/src/components/topBar/TopBar.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import { Logo } from "@/components/icons";
+import { Colors } from "@/styles/colors";
+import { AccountButton } from "@/components/topBar/AccountButton";
+
+export const TopBar = () => {
+  return (
+    <TopBarContainer>
+      <HomeLink href="/">
+        <Logo />
+      </HomeLink>
+      <AccountButton />
+    </TopBarContainer>
+  )
+}
+
+const TopBarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 68px;
+  background-color: ${Colors.White};
+  position: sticky;
+  top: 0;
+  z-index: 99;
+`
+const HomeLink = styled.a`
+  line-height: 1;
+`
--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -2,14 +2,25 @@ import { GlobalStyles } from '@/styles/GobalStyles'
 import type { AppProps } from 'next/app'
 import { BlockchainProviders } from '@/providers/wagmi'
 import { BidsProvider } from '@/providers/BidsProvider/provider'
+import { ReactNode } from "react";
+import { TopBar } from "@/components/topBar/TopBar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <BlockchainProviders>
       <BidsProvider>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        <GlobalStyles/>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </BidsProvider>
     </BlockchainProviders>
   )
 }
+
+const Layout = ({ children }: { children: ReactNode }) => (
+  <>
+    <TopBar/>
+    <main>{children}</main>
+  </>
+)
