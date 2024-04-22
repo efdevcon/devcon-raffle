9dd276ba88b45d25b299bf4c60394f93ad750e80
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-22 16:32:22 +0200

    Add AccountButton

---
+++ packages/frontend/src/components/topBar/AccountButton.tsx
@@ -0,0 +1,25 @@
+import { useAccount } from "wagmi";
+import { useState } from "react";
+import { Button } from "@/components/buttons/Button";
+import { shortenEthAddress } from "@/utils/formatters/shortenEthAddress";
+import { ConnectWalletButton } from "@/components/buttons/ConnectWalletButton";
+import { AccountDetailModal } from "@/components/topBar/AccountDetailModal";
+
+export const AccountButton = () => {
+  const { address } = useAccount()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  return (
+    <>
+      {address ? (
+        <Button view="secondary" onClick={() => setIsModalOpen(!isModalOpen)}>
+          {shortenEthAddress(address)}
+        </Button>
+      ) : (
+        <ConnectWalletButton view="secondary"/>
+      )}
+      {address && isModalOpen &&
+        <AccountDetailModal isShown={isModalOpen} onRequestClose={() => setIsModalOpen(false)}/>}
+    </>
+  )
+}
