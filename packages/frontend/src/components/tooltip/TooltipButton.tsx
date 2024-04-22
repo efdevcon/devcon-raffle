19c305ab9dc3ebaa4ba88c1af8d6fb7da951eeaa
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-22 15:43:48 +0200

    Add tooltip

---
+++ packages/frontend/src/components/tooltip/TooltipButton.tsx
@@ -0,0 +1,35 @@
+import { ReactNode, MouseEvent } from 'react'
+import styled from 'styled-components'
+import { Colors } from "@/styles/colors";
+import { Button } from "@/components/buttons";
+
+interface TooltipButtonProps {
+  color?: string
+  label?: string
+  children?: ReactNode
+  onClick?: (event?: MouseEvent<HTMLButtonElement>) => void
+  onMouseDown?: (event?: MouseEvent<HTMLButtonElement>) => void
+}
+
+export const TooltipButton = ({ color = Colors.Grey, label, children, ...props }: TooltipButtonProps) => {
+  return (
+    <TooltipButtonComponent color={color} label={label} {...props}>
+      {children}
+    </TooltipButtonComponent>
+  )
+}
+
+const TooltipButtonComponent = styled(Button)<Pick<TooltipButtonProps, 'color' | 'label'>>`
+  color: ${({ color }) => color};
+  background-color: ${Colors.White};
+  width: max-content;
+  height: 100%;
+  padding: ${({ label }) => (label ? '4px 12px 4px 4px' : '0')};
+  transition: all 0.25s ease;
+
+  &:hover,
+  &:focus-visible {
+    color: ${Colors.BlueDark};
+    background-color: ${Colors.GreenLight};
+  }
+`
