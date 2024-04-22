520a5d1302aed91a618a23c33bae3cd58adfc9e0
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-22 16:52:44 +0200

    Adjust AccountDetailModal to wagmi hooks

--- packages/frontend/src/components/topBar/AccountDetailModal.tsx
+++ packages/frontend/src/components/topBar/AccountDetailModal.tsx
@@ -1,7 +1,5 @@
-import { useRef, useState } from 'react'
 import styled from 'styled-components'
-import { useWeb3Modal } from "@web3modal/wagmi/react";
-import { useAccount, useChainId, useDisconnect } from "wagmi";
+import { useAccount, useDisconnect } from "wagmi";
 import { Colors } from "@/styles/colors";
 import { Button } from "@/components/buttons";
 import { Modal } from "@/components/topBar/Modal";
@@ -16,44 +14,26 @@ export interface ModalProps {
 }
 
 export const AccountDetailModal = ({ isShown, onRequestClose }: ModalProps) => {
-  const {open, close} = useWeb3Modal()
   const { connector, address } = useAccount()
-  const accountIconRef = useRef<any>(null)
-  const chainId = useChainId()
-  const [wallet, setWallet] = useState('-')
   const explorerLink = useExplorerAddressLink(address)
-  const {disconnect} = useDisconnect()
-  // const { isBraveWallet } = useWhichWallet()
+  const { disconnectAsync } = useDisconnect()
 
-  // useEffect(() => {
-  //   setWallet(getWalletName(web3Modal.cachedProvider, isBraveWallet))
-  //   if (account && accountIconRef.current) {
-  //     accountIconRef.current.innerHTML = ''
-  //     accountIconRef.current.appendChild(Jazzicon(40, parseInt(account.slice(2, 10), 16)))
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [account])
-  //
-  // const onDisconnect = useCallback(() => {
-  //   onRequestClose()
-  //   localStorage.removeItem('walletconnect')
-  //   removeWalletLinkStorage()
-  //   web3Modal.clearCachedProvider()
-  //   deactivate()
-  // }, [onRequestClose, deactivate, web3Modal])
+  const onDisconnect = async () => {
+    await disconnectAsync()
+    onRequestClose()
+  }
 
   return (
     <Modal isShown={isShown} onRequestClose={onRequestClose} title="Your account">
       <ContentWrapper>
         <ContentRow>
-          <ConnectedWallet>Connected with {wallet}</ConnectedWallet>
+          <ConnectedWallet>Connected with {connector?.name}</ConnectedWallet>
         </ContentRow>
         {address && (
           <>
-            <ContentRow>
-              <AccountIcon ref={accountIconRef} />
+            <CenteredContentRow>
               <AccountAddress>{shortenEthAddress(address)}</AccountAddress>
-            </ContentRow>
+            </CenteredContentRow>
             <ContentRow>
               <RedirectButton
                 link={explorerLink ?? ''}
@@ -62,7 +42,6 @@ export const AccountDetailModal = ({ isShown, onRequestClose }: ModalProps) => {
                 label=" View in block explorer"
                 side="top"
               />
-
               <CopyButton
                 value={address}
                 text="Copy account address"
@@ -74,21 +53,12 @@ export const AccountDetailModal = ({ isShown, onRequestClose }: ModalProps) => {
           </>
         )}
       </ContentWrapper>
-      <Button view="secondary" onClick={() => disconnect()}>
+      <Button view="secondary" onClick={onDisconnect}>
         Disconnect
       </Button>
     </Modal>
   )
 }
-
-const AccountIcon = styled.div`
-  display: flex;
-  place-items: center;
-  height: 40px;
-  width: 40px;
-  background-color: ${Colors.Blue};
-  border-radius: 50%;
-`
 
 const AccountAddress = styled.p`
   font-family: 'Space Mono', 'Roboto Mono', monospace;
@@ -110,6 +80,10 @@ const ContentRow = styled.div`
   justify-content: center;
   align-items: center;
   column-gap: 16px;
+`
+
+const CenteredContentRow = styled(ContentRow)`
+  width: 100%;
 `
 
 const ContentWrapper = styled.div`
