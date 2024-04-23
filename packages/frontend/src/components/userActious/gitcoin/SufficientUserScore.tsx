90e4c3c80ffa60c83245c550088f93f4a70e0981
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-23 15:40:38 +0200

    Make sufficient score green

--- packages/frontend/src/components/userActious/gitcoin/SufficientUserScore.tsx
+++ packages/frontend/src/components/userActious/gitcoin/SufficientUserScore.tsx
@@ -2,11 +2,13 @@ import { FormHeading, FormRow, FormWrapper } from '@/components/form'
 import { Button } from '@/components/buttons'
 import { UserScoreProps } from '@/components/userActious/gitcoin/UserGitcoinScore'
 import { environment } from '@/config/environment'
+import styled from "styled-components";
+import { Colors } from "@/styles/colors";
 
 export const SufficientUserScore = ({ userScore }: UserScoreProps) => {
   return (
     <FormWrapper>
-      <FormHeading>Your Score: {userScore}</FormHeading>
+      <FormHeading>Your Score:&nbsp;<Score>{userScore}</Score></FormHeading>
       <FormRow>
         <p>
           To place a bid your score needs to be <b>at least {environment.gitcoinRequiredScore}</b>.
@@ -19,3 +21,7 @@ export const SufficientUserScore = ({ userScore }: UserScoreProps) => {
     </FormWrapper>
   )
 }
+
+const Score = styled.h2`
+  color: ${Colors.Green};
+`
