a96edc9ce0c49cd056b6f7ed74f4524447204d85
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-23 15:41:02 +0200

    Lint

--- packages/frontend/src/components/userActious/gitcoin/SufficientUserScore.tsx
+++ packages/frontend/src/components/userActious/gitcoin/SufficientUserScore.tsx
@@ -2,13 +2,15 @@ import { FormHeading, FormRow, FormWrapper } from '@/components/form'
 import { Button } from '@/components/buttons'
 import { UserScoreProps } from '@/components/userActious/gitcoin/UserGitcoinScore'
 import { environment } from '@/config/environment'
-import styled from "styled-components";
-import { Colors } from "@/styles/colors";
+import styled from 'styled-components'
+import { Colors } from '@/styles/colors'
 
 export const SufficientUserScore = ({ userScore }: UserScoreProps) => {
   return (
     <FormWrapper>
-      <FormHeading>Your Score:&nbsp;<Score>{userScore}</Score></FormHeading>
+      <FormHeading>
+        Your Score:&nbsp;<Score>{userScore}</Score>
+      </FormHeading>
       <FormRow>
         <p>
           To place a bid your score needs to be <b>at least {environment.gitcoinRequiredScore}</b>.
