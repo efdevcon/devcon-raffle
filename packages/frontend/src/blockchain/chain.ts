d0ef5bdd0d295a1ac43337d5ed349b3f7e1619b9
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-24 11:30:50 +0200

    Allow only for arbitrum on production

--- packages/frontend/src/blockchain/chain.ts
+++ packages/frontend/src/blockchain/chain.ts
@@ -1,4 +1,6 @@
 import { arbitrum, arbitrumSepolia, hardhat } from 'wagmi/chains'
 
-const SUPPORTED_CHAIN_IDS = [arbitrum.id, arbitrumSepolia.id, hardhat.id] as const
-export type SupportedChainId = typeof SUPPORTED_CHAIN_IDS[number]
+const AllSupportedChains = [hardhat, arbitrum, arbitrumSepolia] as const
+export type SupportedChainId = typeof AllSupportedChains[number]['id']
+
+export const SupportedChains = process.env.NODE_ENV === 'production' ? [arbitrum] as const : AllSupportedChains
