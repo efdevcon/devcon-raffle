f84f2de1f001246a63deef250ddfddeae3c1cb6a
Andrzej <andrzej.daniel@archblock.com>
2024-04-25 16:17:44 +0200

    Refactor useUserBid place field

--- packages/frontend/src/blockchain/hooks/useUserBid.ts
+++ packages/frontend/src/blockchain/hooks/useUserBid.ts
@@ -1,19 +1,56 @@
 import { useAccount } from 'wagmi'
 import { UserBid } from '@/types/bid'
 import { useBidWinType } from './useBidWinType'
-import { useBid } from './useBid'
+import { useChainId, useReadContract } from 'wagmi'
+import { AUCTION_ABI } from '@/blockchain/abi/auction'
+import { AUCTION_ADDRESSES } from '@/blockchain/auctionAddresses'
+import { Hex } from 'viem'
+import { useMemo } from 'react'
+import { useBids } from '@/providers/BidsProvider'
 
 export const useUserBid = (): UserBid | undefined => {
   const { address } = useAccount()
-  const { bid } = useBid(address)
+  const chainId = useChainId()
+  const { bidList } = useBids()
+
+  const bid = useMemo(() => {
+    if (!address) {
+      return undefined
+    }
+
+    const userBidPlace = bidList.findIndex((bid) => bid.address === address)
+    return userBidPlace >= 0
+      ? {
+          ...bidList[userBidPlace],
+          place: userBidPlace + 1,
+        }
+      : undefined
+  }, [address, bidList])
+
+  const { data } = useReadContract({
+    chainId,
+    abi: AUCTION_ABI,
+    address: AUCTION_ADDRESSES[chainId],
+    functionName: 'getBid',
+    args: [address as Hex],
+    query: {
+      enabled: !!address,
+    },
+  })
+  const claimed = data?.claimed
+
   const winType = useBidWinType(bid?.bidderId)
 
-  if (winType == undefined || !bid) {
+  if (winType === undefined || !bid || claimed === undefined || !address) {
     return undefined
   }
 
   return {
-    ...bid,
+    address,
+    amount: bid.amount,
+    bidderId: bid.bidderId,
+    place: bid.place,
+    claimed,
     winType,
   }
 }
