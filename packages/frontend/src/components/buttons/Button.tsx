48c75f7c72ee9f63cbb52e04ec75816f285e9cd5
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-04-26 14:54:30 +0200

    Resolve transient props in stepper

--- packages/frontend/src/components/buttons/Button.tsx
+++ packages/frontend/src/components/buttons/Button.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 import styled, { css } from 'styled-components'
 import { Colors } from '@/styles/colors'
 import { LoadingIcon } from '@/components/icons'
-import { TransientProps } from "@/styles/transientProps";
+import { TransientProps } from '@/styles/transientProps'
 
 export interface ButtonProps {
   children: ReactNode
