2185855ee593abf3bb853c4bcc50fd2932414995
Kevin Charm <kevin@kevincharm.com>
2024-04-27 01:34:42 +0200

    add cors rules to next config

--- packages/frontend/next.config.mjs
+++ packages/frontend/next.config.mjs
@@ -4,6 +4,22 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-};
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: process.env.WHITELISTED_ORIGINS },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,PUT,POST,DELETE,OPTIONS' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization, Cookie',
+          },
+        ],
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
