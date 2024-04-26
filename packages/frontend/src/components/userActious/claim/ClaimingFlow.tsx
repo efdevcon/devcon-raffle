75861c08d411f61b959d7752dfc0b75fe95f0b2d
pazerny kormoran <38940023+pazernykormoran@users.noreply.github.com>
2024-04-26 11:57:36 +0200

    🔮 Add claimed field to useUserBid (#37)
    Co-authored-by: Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>

--- packages/frontend/src/components/userActious/claim/ClaimingFlow.tsx
+++ packages/frontend/src/components/userActious/claim/ClaimingFlow.tsx
@@ -1,11 +1,9 @@
-import { useUserBid } from '@/blockchain/hooks/useUserBid'
 import { NoBidding } from '@/components/userActious/claim/NoBidding'
 import { WinBidFlow } from '@/components/userActious/claim/WinBidFlow'
-import { WinType } from '@/types/winType'
+import { useUserSettledBid } from '@/blockchain/hooks/useUserSettledBid'
 
 export const ClaimingFlow = () => {
-  const bid = useUserBid()
-  const userBid = bid ? { ...bid, winType: WinType.GoldenTicket, claimed: false } : undefined
+  const userBid = useUserSettledBid()
 
   return userBid ? <WinBidFlow userBid={userBid} /> : <NoBidding />
 }
