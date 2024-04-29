344217a2ef8b0d546f64cd8bc5ab1b7198d62edf
Kevin Charm <kevin@kevincharm.com>
2024-04-30 01:07:19 +0200

    replace loadEnv with dotenv

--- packages/frontend/vitest.config.ts
+++ packages/frontend/vitest.config.ts
@@ -1,9 +1,9 @@
-import { loadEnv } from 'vite'
+import dotenv from 'dotenv'
 import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 const setup = ({ mode }: { mode: string }) => {
-  Object.assign(process.env, loadEnv(mode, './', ''))
+  dotenv.config({ path: `.env.${mode}` })
   return defineConfig({
     plugins: [tsconfigPaths()],
     test: {
