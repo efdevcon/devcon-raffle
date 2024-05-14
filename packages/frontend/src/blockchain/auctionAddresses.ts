854dd7e13d75657f3de9ff61d143764a8bcf3822
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-05-14 15:39:37 +0200

    Set latest Arbitrum Sepolia deployment

--- packages/frontend/src/blockchain/auctionAddresses.ts
+++ packages/frontend/src/blockchain/auctionAddresses.ts
@@ -4,6 +4,6 @@ import { arbitrum, arbitrumSepolia, hardhat } from 'wagmi/chains'
 
 export const AUCTION_ADDRESSES: Record<SupportedChainId, Hex> = {
   [arbitrum.id]: '0xF53d383525117d1f51BF234966E39bD1508a5948',
-  [arbitrumSepolia.id]: '0x426Ee70332Ae67a5FdFb7B4B4A2C91F1f51712b0',
+  [arbitrumSepolia.id]: '0x2cc7f85730052dA7389e276115975a5dB1e7A668',
   [hardhat.id]: '0xa513e6e4b8f2a923d98304ec87f64353c4d5c853',
 }
