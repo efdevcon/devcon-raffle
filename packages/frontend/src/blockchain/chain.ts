f9f81f7ea3587d296eb5afca295894d81ba469d2
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-05-14 16:41:46 +0200

    Allow for arbitrumSepolia

--- packages/frontend/src/blockchain/chain.ts
+++ packages/frontend/src/blockchain/chain.ts
@@ -3,4 +3,5 @@ import { arbitrum, arbitrumSepolia, hardhat } from 'wagmi/chains'
 const AllSupportedChains = [hardhat, arbitrum, arbitrumSepolia] as const
 export type SupportedChainId = (typeof AllSupportedChains)[number]['id']
 
-export const SupportedChains = process.env.NODE_ENV === 'production' ? ([arbitrum] as const) : AllSupportedChains
+export const SupportedChains =
+  process.env.NODE_ENV === 'production' ? ([arbitrumSepolia, arbitrum] as const) : AllSupportedChains
