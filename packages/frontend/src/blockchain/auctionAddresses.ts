930ff70170f5c4bcc0a2f553cc68d32d366c7bc0
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-06-17 20:55:39 +0200

    Set auction production address

--- packages/frontend/src/blockchain/auctionAddresses.ts
+++ packages/frontend/src/blockchain/auctionAddresses.ts
@@ -3,7 +3,7 @@ import { Hex } from 'viem'
 import { arbitrum, arbitrumSepolia, hardhat } from 'wagmi/chains'
 
 export const AUCTION_ADDRESSES: Record<SupportedChainId, Hex> = {
-  [arbitrum.id]: '0xF53d383525117d1f51BF234966E39bD1508a5948',
+  [arbitrum.id]: '0xe2EF0Cb2A3ccf8107D28f01dC721055225D5fedA',
   [arbitrumSepolia.id]: '0x2cc7f85730052dA7389e276115975a5dB1e7A668',
   [hardhat.id]: '0xa513e6e4b8f2a923d98304ec87f64353c4d5c853',
 }
