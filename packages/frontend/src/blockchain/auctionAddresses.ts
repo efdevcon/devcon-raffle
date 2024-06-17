97db1087b43684e68f3f54adbb77d8774758bd5c
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-06-17 21:22:42 +0200

    Set devcon address on staging

--- packages/frontend/src/blockchain/auctionAddresses.ts
+++ packages/frontend/src/blockchain/auctionAddresses.ts
@@ -4,6 +4,6 @@ import { arbitrum, arbitrumSepolia, hardhat } from 'wagmi/chains'
 
 export const AUCTION_ADDRESSES: Record<SupportedChainId, Hex> = {
   [arbitrum.id]: '0xe2EF0Cb2A3ccf8107D28f01dC721055225D5fedA',
-  [arbitrumSepolia.id]: '0x2cc7f85730052dA7389e276115975a5dB1e7A668',
+  [arbitrumSepolia.id]: '0xDF3e430096eF44a6b37fDAC8E345F2314AccEB9F',
   [hardhat.id]: '0xa513e6e4b8f2a923d98304ec87f64353c4d5c853',
 }
