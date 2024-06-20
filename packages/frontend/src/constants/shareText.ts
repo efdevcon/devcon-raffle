e4ea939f4436906984a5bca43bd4528a9d842e3b
Maria Rushkova <maria.rushkova@trusttoken.com>
2024-06-20 12:18:09 +0200

    Update share message

--- packages/frontend/src/constants/shareText.ts
+++ packages/frontend/src/constants/shareText.ts
@@ -1,2 +1,2 @@
 export const shareText =
-  'I placed a bid in the Devcon 7 Ticket Raffle-Auction! Join the raffle here https://raffle.devcon.org/'
+  'I placed a bid in the @EFDevcon 7 Ticket Raffle-Auction! Join the raffle here: https://raffle.devcon.org/'
