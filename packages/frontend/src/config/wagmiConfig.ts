6908823746dd00c926a09cf51a1ae0b64ec7c583
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-07-10 10:34:08 +0200

    Use http transports in wagmi config

--- packages/frontend/src/config/wagmiConfig.ts
+++ packages/frontend/src/config/wagmiConfig.ts
@@ -1,4 +1,4 @@
-import { cookieStorage, createConfig, createStorage, webSocket } from 'wagmi'
+import { cookieStorage, createConfig, createStorage, http } from 'wagmi'
 import { arbitrum, arbitrumSepolia, hardhat } from 'wagmi/chains'
 import { coinbaseWallet, walletConnect } from 'wagmi/connectors'
 import { environment } from '@/config/environment'
@@ -12,9 +12,9 @@ export const wagmiConfig = createConfig({
   chains: SupportedChains,
   ssr: true,
   transports: {
-    [arbitrum.id]: webSocket(`wss://arbitrum-mainnet.infura.io/ws/v3/${environment.infuraKey}`),
-    [arbitrumSepolia.id]: webSocket(`wss://arbitrum-sepolia.infura.io/ws/v3/${environment.infuraKey}`),
-    [hardhat.id]: webSocket('ws://127.0.0.1:8545'),
+    [arbitrum.id]: http(`https://arbitrum-mainnet.infura.io/v3/${environment.infuraKey}`),
+    [arbitrumSepolia.id]: http(`https://arbitrum-sepolia.infura.io/v3/${environment.infuraKey}`),
+    [hardhat.id]: http('http://127.0.0.1:8545'),
   },
   connectors: [
     walletConnect({ projectId: environment.walletConnectProjectId }),
