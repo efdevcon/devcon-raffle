679554b71538a6173be44b19c5a2746f852b6c45
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2024-08-02 22:51:46 +0200

    ✨ Fetch winners in claiming closed state (#91)

--- packages/frontend/src/components/bids/allBids/SettledBidsList.tsx
+++ packages/frontend/src/components/bids/allBids/SettledBidsList.tsx
@@ -22,11 +22,11 @@ interface SettledBidsListProps {
 export const SettledBidsList = ({ search }: SettledBidsListProps) => {
   const { bidList } = useBids()
   const matchesSearch = bidMatchesSearch(search)
-  const { auctionWinners, raffleWinners } = useAuctionWinners()
+  const { auctionWinners, raffleWinners, goldenWinner } = useAuctionWinners()
 
   const settledBids = useMemo(
-    () => divideBids(bidList, auctionWinners, raffleWinners),
-    [bidList, auctionWinners, raffleWinners],
+    () => divideBids(bidList, auctionWinners, raffleWinners, goldenWinner),
+    [bidList, auctionWinners, raffleWinners, goldenWinner],
   )
 
   const filteredBids = useMemo(() => filterBids(settledBids, matchesSearch), [settledBids, matchesSearch])
@@ -52,6 +52,7 @@ function divideBids(
   bids: Bid[],
   auctionWinners: readonly bigint[] | undefined,
   raffleWinners: readonly bigint[] | undefined,
+  goldenWinner: bigint | undefined,
 ): Bids {
   const settledBids: Bids = {
     auction: [],
@@ -69,7 +70,7 @@ function divideBids(
       settledBids.auction.push(bid)
       return
     }
-    if (bidderID === raffleWinners[0]) {
+    if (bidderID === goldenWinner) {
       settledBids.goldenTicket = bid
       return
     }
